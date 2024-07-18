1b3dc5b9c5b1b8a55d3ff50fcd236eca0eb6f3bc
Taoheedoh <taoheedoh@gmail.com>
2024-07-18 13:53:55 +0100

    ADD: Replay

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,8 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
